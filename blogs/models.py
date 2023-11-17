6ca077fab5f8f08080ba22c76e54811745c761e6
vaniakazeko <kazeko.ivan.00@gmail.com>
2023-11-17 13:26:25 +0300

    blog

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -43,6 +44,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=250, verbose_name='Название')
     date_added = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     description = models.TextField(verbose_name='Описание')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', null=True)
 
     def __str__(self):
         return self.title
